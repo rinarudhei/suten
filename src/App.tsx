3d529fc39d37769234f3ec1469c064669f211c5d
rinarudhei <rinaldiadrian5@gmail.com>
2026-04-02 08:29:19 +0700

    chore: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from 'react-router';
 import './App.scss';
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path='/' element={<></>} />
+      <Route path='/big-bang-theory' element={<></>} />
+      <Route path='/playground' element={<></>} />
+    </Routes>
+  );
 }
 
 export default App;
