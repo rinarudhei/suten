6b8971e3395d18c2a910e8e4ffcb1528c00a94dd
rinarudhei <rinaldiadrian5@gmail.com>
2026-04-08 22:24:46 +0700

    chore: remove playground route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { Routes, Route } from 'react-router';
 import './App.scss';
 import SutenGame from './components/templates/SutenGame';
-import PreStepTwo from './components/organisms/PreStepTwo';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<SutenGame />} />
       <Route path='/big-bang-theory' element={<></>} />
-      {import.meta.env.DEV && (
-        <Route
-          path='/playground'
-          element={<PreStepTwo selectedPiece='paper' />}
-        />
-      )}
     </Routes>
   );
 }
